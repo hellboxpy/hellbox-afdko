df476113fba4791cf4e7b8b9d9b5fdf9ca6ce1a0
Jack Jennings <j@ckjennin.gs>
2026-05-06 20:39:07 -0700

    feat: export MakeOTF, Otf2Ttf, TtfComponentizer from package

---
+++ src/hellbox/jobs/afdko/__init__.py
@@ -0,0 +1,5 @@
+from hellbox.jobs.afdko.make_otf import MakeOTF
+from hellbox.jobs.afdko.otf2ttf import Otf2Ttf
+from hellbox.jobs.afdko.ttf_componentizer import TtfComponentizer
+
+__all__ = ["MakeOTF", "Otf2Ttf", "TtfComponentizer"]
